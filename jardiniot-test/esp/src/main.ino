0e312bb53cbc9fb68f345895c47ce39efb404d25
AXDOOMER <alexandrexavier@live.ca>
2017-02-20 21:50:06 -0500

    Projet(s) qui compile(nt) sans erreur

--- jardiniot-test/esp/src/main.ino
+++ jardiniot-test/esp/src/main.ino
@@ -1,9 +1,31 @@
-# CODE POUR LE ESP
+#include <SoftwareSerial.h>
+SoftwareSerial ESPserial(3, 4); // RX | TX
 
-void setup() {
+void setup()
+{
+    Serial.begin(9600);    // communication with the host computer
+    //while (!Serial)   { ; }
 
+    // Start the software serial for communication with the ESP8266
+    ESPserial.begin(57600);
+
+    Serial.println("");
+    ESPserial.println("Remember to to set Both NL & CR in the serial monitor.");
+    ESPserial.println("Ready");
+    ESPserial.println("");
 }
 
-void loop() {
+void loop()
+{
+    // listen for communication from the ESP8266 and then write it to the serial monitor
+    if (ESPserial.available())
+	{
+		Serial.write(ESPserial.read());
+	}
 
+    // listen for user input and send it to the ESP8266
+    if (Serial.available())
+	{
+		ESPserial.write(Serial.read());
+	}
 }
