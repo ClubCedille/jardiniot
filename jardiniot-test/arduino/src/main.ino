67ba107d8a7b2a397a130912fc58c59a3fd98f1d
AXDOOMER <alexandrexavier@live.ca>
2017-02-20 21:50:06 -0500

    Projet(s) qui compile(nt) sans erreur

--- jardiniot-test/arduino/src/main.ino
+++ jardiniot-test/arduino/src/main.ino
@@ -1,9 +1,21 @@
-# CODE POUR LE ARDUINO
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 4); // RX, TX
 
 void setup() {
-
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  while (!Serial) {
+     ; // wait for serial port to connect. Needed for native USB port only
+   }
 }
 
 void loop() {
-
+  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+  while (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
 }
