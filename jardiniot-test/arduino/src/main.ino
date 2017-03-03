ede59102f085054fa7f11c5d99eec3336ea1ae09
AXDOOMER <alexandrexavier@live.ca>
2017-03-02 20:51:10 -0500

    Code qui read/write le input de la console + flash la DEL

--- jardiniot-test/arduino/src/main.ino
+++ jardiniot-test/arduino/src/main.ino
@@ -1,21 +1,31 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(3, 4); // RX, TX
+/* Code pour le Arduino Uno */
+/* Copyright Alexandre-Xavier Labonté-Lamoureux, 2017
+ * Distributed under the BSD license
+*/
+int ledPin = 13;                 // LED connected to digital pin 13
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(57600);
+  Serial.begin(9600);
   while (!Serial) {
-     ; // wait for serial port to connect. Needed for native USB port only
-   }
+      ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.println("Ready soon!");
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available()) {
-    mySerial.write(Serial.read());
-  }
-  while (mySerial.available()) {
-    Serial.write(mySerial.read());
+
+  digitalWrite(ledPin, LOW);
+  delay(10);
+
+  if (Serial.available()) {
+    byte byteRead = Serial.read();
+    Serial.write(byteRead);
+    digitalWrite(ledPin, HIGH);
+    delay(10);
   }
 }
