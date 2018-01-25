ccf78fa93b99f4aac6ea2afcde67227478254584
notarock <roch.damour@gmail.com>
2018-01-25 00:37:35 -0500

    App header

---
+++ frontend-web/src/app/bucket/bucket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket',
+  templateUrl: './bucket.component.html',
+  styleUrls: ['./bucket.component.css']
+})
+export class BucketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
