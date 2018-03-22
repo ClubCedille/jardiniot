483e10caa651c69d05622b64ce110e7bab485937
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2018-03-22 00:31:45 -0400

    On peut envoyer des commandes du nouveau format et elles seront exécutées
    Voir https://rocketchat.cedille.club/channel/projet_jardiniot?msg=oD3v7G9FkxfLgytDq pour voir comment ça été résolu

--- jardiniot-emb/arduino/src/main.cpp
+++ jardiniot-emb/arduino/src/main.cpp
@@ -17,120 +17,120 @@
 #include <include/CommandManager.h>
 #include <SoftwareSerial.h>
 #include <stdlib.h> /* strtoul */
+#include "Timer.h"
 
 // http://www.martyncurrey.com/arduino-to-esp8266-serial-commincation/
-// SoftwareSerial ESPserial(3, 4); // pin 3 à TX du ESP | pin 4 à RX du ESP
+SoftwareSerial ESPserial(3, 4); // pin 3 à TX du ESP | pin 4 à RX du ESP
+Timer timer;
+int k = 0;	// Tics counter
+CommandManager* cm;
 
 // Fonction utilisée pour connaitre la RAM libre sur le Arduino lorsque la RAM est pleine le arduino bloc et doit être redémarré
-int freeRam ()
-{
+int freeRam() {
   extern int __heap_start, *__brkval;
   int v;
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
-CommandManager* cm;
+// Envoie de mock data
+void sendStatusToESP() {
+	// Send data to ESP8266
+	char sensorStatus[80];
 
-int k = 0;
+	// On met la valeur du tic dans le tableau de chars
+	char s[12];
+	sprintf(s,"%d", k);
+
+	sprintf(sensorStatus, "\"temperature\": 24.20,\"humidite\": 30.40,\"tics\":,\"tic\":%s", s);
+
+	ESPserial.write(sensorStatus);
+}
 
 void setup(){
-    Serial.begin(9600);
-    cm = new CommandManager();
+	Serial.begin(9600);
+	ESPserial.begin(9600);
+	while (!Serial) {
+		; // wait for serial port to connect. Needed for native USB port only
+	}
 
-    // ESPserial.begin(9600);
-    // while (!Serial) {
-    //   ; // wait for serial port to connect. Needed for native USB port only
-    // }
+	Serial.println(F("Serial pret."));
+
+	cm = new CommandManager();
+	Serial.println(F("Command manager pret."));
+
+	/******************* On met en place la configuration par défaut *******************/
+
+	// Lumières DEL se trouvant sous le heatsink
+	short red = 11*256+125;	// Pin 11 avec intensité de 125, de 2816 à 3071
+	short blue = 10*256+160;	// Pin 10 avec intensité de 160, de 2560 à 2815
+	short white = 9*256+50;	// Pin 9 avec intensité de 50, de 2304 à 2559
+	cm->executeCommand("id 1 a 2 200 i " + String(white) + " " + String(blue) + " " + String(red));
+
+	// Ajout de FAN
+	cm->executeCommand("id 5 a 4 250 i " + String(5*256+255));		// Fan latérale (pin 5), de 1280 à 1535
+	cm->executeCommand("id 6 a 4 250 i " + String(6*256+255));		// Fan du heatsink, de 1536 à 1791
+
+	// Ajout d'un DHT
+	// TODO: Si decommenté, cause problèmes de mémoire ou de socket sur le ESP
+	//cm->executeCommand("id 7 a 0 250 i " + String(2*256+22));
+
+	Serial.println(F("Config par defaut executee."));
+
+	// À chaque 2 secondes fait un send des infos
+	int idTimerEvent = timer.every(2000, sendStatusToESP);
+	Serial.println(F("Timer pret."));
 }
 
 // À Décommenter une fois que le ESP sera connecté
-// void readInfoFromESP()
-// {
-//   if (ESPserial.available()) {
-//     String command = ESPserial.readString();
-//     // Si la chaine de caractère n'est pas vide
-//     if(command.length() != 0){
-//       Serial.print(F("Value received: "));
-//       Serial.println(command);
-//
-//       cm->executeCommand(command);
-//     }
-//   }
-// }
+void readInfoFromESP()
+{
+	if (ESPserial.available()) {
+		String command = ESPserial.readString();
+		// Si la chaine de caractère n'est pas vide
+		if(command.length() != 0) {
+			Serial.print(F("Value received: "));
+			Serial.print(command);
+			Serial.println(F("."));
 
-// TO BE REMOVED
-void testArduino(){
-	if(k == 0)
-	{
-		// Lumières DEL se trouvant sous le heatsink
-		short red = 11*256+125;	// Pin 11 avec intensité de 125
-		short blue = 10*256+160;	// Pin 10 avec intensité de 160
-		short white = 9*256+50;	// Pin 9 avec intensité de 50
-		cm->executeCommand("id 1 a 2 200 i " + String(white) + " " + String(blue) + " " + String(red));
-
-		// Ajout de FAN
-		cm->executeCommand("id 5 a 4 250 i " + String(5*256+255));		// Fan latérale (pin 5)
-		cm->executeCommand("id 6 a 4 250 i " + String(6*256+255));		// Fan du heatsink
-
-		// Ajout d'un DHT
-		cm->executeCommand("id 7 a 0 250 i " + String(2*256+22));
+			if (command[0] == 'i' && command[1] == 'd') {
+				cm->executeCommand(command);
+			}
+		}
 	}
-	// Ces deux prochaines conditions font flasher les lumières
-	else if (k > 10 && k / 10 % 2 == 0)
-	{
-		short red = 11*256+10;	// Pin 11 avec intensité de 10
-		short blue = 10*256+30;	// Pin 10 avec intensité de 30
-		short white = 9*256+10;	// Pin 9 avec intensité de 10
-		cm->executeCommand("id 1 c 2 200 i " + String(white) + " " + String(blue) + " " + String(red));
-
-		cm->executeCommand("id 5 c 4 250 i " + String(5*256+255));
-		cm->executeCommand("id 6 c 4 250 i " + String(6*256+255));
-	}
-	else if (k > 10 && k / 10 % 2 == 1)
-	{
-		short red = 11*256+125;	// Pin 11 avec intensité de 125
-		short blue = 10*256+160;	// Pin 10 avec intensité de 160
-		short white = 9*256+50;	// Pin 9 avec intensité de 50
-		cm->executeCommand("id 1 c 2 200 i " + String(white) + " " + String(blue) + " " + String(red));
-
-		cm->executeCommand("id 5 c 4 250 i " + String(5*256+0));
-		cm->executeCommand("id 6 c 4 250 i " + String(6*256+0));
-	}
-
-	k++;
 }
 
-void loop(){
+void loop() {
+	// Éliminer le plus possible les serial.print puisqu'il coûte cher en mémoire
+	// Si nécessaire utiliser F() pour storer les chaines de caractère dans la mémoire Flash au lieu de la mettre dans la mémoire Ram
+	Serial.print(F("Free memory "));
+	Serial.println(freeRam());
 
-    // Éliminer le plus possible les serial.print puisqu'il coûte cher en mémoire
-    // Si nécessaire utiliser F() pour storer les chaines de caractère dans la mémoire Flash au lieu de la mettre dans la mémoire Ram
-    Serial.print(F("=========== Free memory "));
-    Serial.println(freeRam());
+	timer.update();
 
-    // Utiliser pour les tests seulement
-    testArduino();
+	// Boucle de gestion pour les sensors
+	for(unsigned char i = 0; i < cm->getSensorList().size(); i++) {
+		// Écrit les valeurs au pin spécifié lors de la création/configuration
+		cm->getSensorList()[i]->write();
 
-    // Boucle de gestion pour les sensors
-    for(unsigned char i = 0; i < cm->getSensorList().size(); i++){
-        // Écrit les valeurs au pin spécifié lors de la création/configuration
-        cm->getSensorList()[i]->write();
+		char* status = cm->getSensorList()[i]->read();
 
-        char* status = cm->getSensorList()[i]->read();
-        if((status != NULL) && (status[0] != '\0')){
-            // Test
-            Serial.println(status);
+		// TODO: À tester avec le DHT
+		if ((status != NULL) && (status[0] != '\0')) {
+			// Test
+			Serial.println(status);
 
-            // Send data to ESP !!!! À Décommenter !!!!
-            // ESPserial.write(status);
-        }
-    }
+			// Send data to ESP
+			//ESPserial.write(status);
+		}
+	}
 
-    // Boucle de gestion pour les moteurs
-    for(unsigned char i = 0; i < cm->getMotorList().size(); i++){
-        cm->getMotorList()[i]->activate();
-    }
+	// Boucle de gestion pour les moteurs
+	for(unsigned char i = 0; i < cm->getMotorList().size(); i++){
+		cm->getMotorList()[i]->activate();
+	}
 
-    // Listen for communication from ESP
-    // readInfoFromESP(); !!!! À Décommenter !!!!
+	Serial.println("tic is " + String(k++));
 
+	// Listen for communication from ESP
+	readInfoFromESP();
 }
