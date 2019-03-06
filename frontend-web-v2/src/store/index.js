4081b36c2926a312246a8d1eb5d08f309371a3d5
Nicolas Berbiche <nic.berbiche@gmail.com>
2019-03-05 23:21:20 -0500

    Corrige le bug de page blanche du site web
    Voir <https://github.com/jhen0409/react-native-debugger/issues/280#issuecomment-432298556>

--- frontend-web-v2/src/store/index.js
+++ frontend-web-v2/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose as composeR } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
@@ -13,14 +13,16 @@ const persistConfig = {
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || composeR;
 
 export const configureStore = (history) => {
   const routeMiddleware = routerMiddleware(history);
   let store = createStore(
     connectRouter(history)(persistedReducer),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(routeMiddleware),
-    applyMiddleware(thunk)
+    compose(
+      applyMiddleware(routeMiddleware),
+      applyMiddleware(thunk),
+    ),
   );
 
   let persistor = persistStore(store);
