c37ddea966c5d19021cad7c356d370f4091b160e
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2019-06-26 22:22:16 -0400

    RIP ancien Front-End Web de Roch

--- frontend-web/src/app/sensors/sensor.ts
+++
@@ -1,7 +0,0 @@
-export class Sensor {
-    id: number;
-    bucket_id: number;
-    name: string;
-    type: string;
-    data: Object;
-}
--- frontend-web/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    api_hostname: "http://localhost:8080"
-};
