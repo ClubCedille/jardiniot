cbfd0ffa02f09e5dee65fddc6cd10bb73a481c22
Perceval62 <perrier.vincent0210@gmail.com>
2020-03-23 18:30:36 -0400

    reajout de config.h

---
+++ jardiniot-emb/rev2/main/include/config.h
@@ -0,0 +1,20 @@
+/*
+* Fichier contenant des macros pour la configuration du projet
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+    #define ESP_WIFI_SSID "example"
+    #define ESP_WIFI_PASS "example"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
