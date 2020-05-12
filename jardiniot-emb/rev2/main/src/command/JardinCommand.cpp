94c3686428c42cec0bcb3fae654d6a833f93f3c2
Perceval62 <perrier.vincent0210@gmail.com>
2020-05-12 17:33:04 -0400

    Ajout des headers GPL, comme demandé

--- jardiniot-emb/rev2/main/src/command/JardinCommand.cpp
+++ jardiniot-emb/rev2/main/src/command/JardinCommand.cpp
@@ -1,5 +1,21 @@
-#include "JardinCommand.h"
+// Copyright (C) 2020      Vincent Perrier
+// Copyright (C) 2017-2018 Alexandre-Xavier Labonté-Lamoureux
+// Copyright (C) 2017      Alexandre Brochu
+//
+// JardinIoT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// JardinIoT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with JardinIoT.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "JardinCommand.h"
 
 /**
     Les commandes, qui peuvent être reçues, doivent être du format suivant :
