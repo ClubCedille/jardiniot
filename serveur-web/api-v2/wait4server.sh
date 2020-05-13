865dc19d5d62ef44ccde3415d8d3b4051bf104da
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2020-05-12 22:36:07 -0400

    curl tests

--- serveur-web/api-v2/wait4server.sh
+++ serveur-web/api-v2/wait4server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-while [ "$(curl -I http://127.0.0.1:5000/buckets 2>/dev/null | head -n 1 | cut -d$' ' -f2)" != "200" ]
+while [ "$(curl -s -I http://127.0.0.1:5000/buckets 2>/dev/null | head -n 1 | cut -d$' ' -f2)" != "200" ]
 do
 	echo "waiting for server to start"
-	echo $(curl -I http://127.0.0.1:5000/buckets 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+	echo $(curl -s -I http://127.0.0.1:5000/buckets | head -n 1 | cut -d$' ' -f2)
+	echo $(curl -s -I http://127.0.0.1:5050/buckets | head -n 1 | cut -d$' ' -f2)
+	echo $(curl -s -I http://127.0.0.1:5000/buckets)
 	sleep 1
 done
