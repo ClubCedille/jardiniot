3ec404ef187946c64006bbd53028d87c1ec4662f
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2020-05-12 22:24:50 -0400

    test

--- serveur-web/api-v2/wait4server.sh
+++ serveur-web/api-v2/wait4server.sh
@@ -2,5 +2,7 @@
 
 while [ "$(curl -I http://127.0.0.1:5000/buckets 2>/dev/null | head -n 1 | cut -d$' ' -f2)" != "200" ]
 do
+	echo "waiting for server to start"
+	echo $(curl -I http://127.0.0.1:5000/buckets 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 	sleep 1
 done
