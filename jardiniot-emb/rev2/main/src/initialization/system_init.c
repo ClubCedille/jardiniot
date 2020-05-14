b19a317214e75b1d7939b3e48d922aa29fe2ef9f
perrier.vincent0210@gmail.com <perrier.vincent0210@gmail.com>
2020-05-13 20:16:12 -0400

    Enlevé le code inutile et commenté dans le fichier system_init.c

--- jardiniot-emb/rev2/main/src/initialization/system_init.c
+++ jardiniot-emb/rev2/main/src/initialization/system_init.c
@@ -203,35 +203,3 @@ void wifi_access_point( ) {
     ESP_LOGI( TAG2, "ESP_WIFI_MODE_AP" );
     wifi_init_softap( );
 }
-/*
-void configureIP(unsigned char SSID[32], unsigned char pw[64])
-{
-    #define PORT 80
-    int new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-
-
-    char *hello = "<h1>Hello from server</h1>";
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    bind(sockfd, (struct sockaddr *)&address, sizeof(address));
-    if (listen(sockfd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(sockfd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    printf(buffer);
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("<h1>Hello message sent</h1>");
-}*/
